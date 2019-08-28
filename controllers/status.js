8061503356afbc38222fa661784b92ea2691e1c6
Philip Wisner <philipwisner@gmail.com>
2019-08-28 12:54:05 -0400

    added dynamic channel

--- controllers/status.js
+++ controllers/status.js
@@ -8,9 +8,12 @@ const status = {};
 
 //RETURNS ALL USER'S NAMES AND STATUS FOR SN_DEVS CHANNEL
 status.channel_status = (req, res) => {
+  console.log('req', req.params)
+  let channel = req.params.id;
+
   (async () => {
     const mems = await web.conversations.members({
-      channel: "GDVTDFL4A"
+      channel: channel
     });
 
     let members = mems.members;

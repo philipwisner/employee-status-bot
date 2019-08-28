f01029b0b0728ab0877d5e5903695498db9f5e8a
Philip Wisner <philipwisner@gmail.com>
2019-08-28 12:54:20 -0400

    added route for channel id

--- routes.js
+++ routes.js
@@ -6,10 +6,13 @@ const router = express.Router();
 const bot = require("./controllers/bot");
 const slash = require("./controllers/slash");
 const status = require("./controllers/status")
+const channel = require("./controllers/channel");
 
 //GET APIS
 router.get('/', bot.connect);
 router.get("/status", status.channel_status);
+router.get("/status/:id", status.channel_status);
+router.get("/channels", channel.channel_list);
 
 //POST APIS
 router.post("/bot", bot.bot_message);
